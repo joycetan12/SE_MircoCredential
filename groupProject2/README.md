39598a2892224b4e72ce4f6f4708d1c57e33e55c
Joyce Tan <joycetan1298@gmail.com>
2021-03-08 20:15:37 -0500

    Added group project 2

---
+++ groupProject2/README.md
@@ -0,0 +1,18 @@
+# groupProject2
+
+**Joyce:** Worked on Covid-19 Cases Section.
+- In state-data1.js, I changed the data to represent covid-19 data in every borough of NYC instead of data about the states in the U.S.
+- In MidTermProj.html, I changed the State Facts Section to a Covid-19 Cases Section. I changed the dropdown to show NYC boroughs instead of states in the U.S. I also changed id values accordingly to correctly display the covid-19 data from state-data1.js.
+
+
+**Aleef:** Worked on Design/Navigation.
+- I created the logo and banner and uploaded to the website
+- I also changed color of appropriate elements to suit our color scheme
+- I also worked on changing links on navigation bar
+
+**Oksana:** Worked on Facts Card, Volunteer Application.
+- I resized the card and chenged the content with facts.
+- I fixed the Volunteer Application's option box and changed the content. 
+- I created an alert on Submit button.
+
+**Tisha:** Worked on Paragraphs Section/Content.
---
+++ groupProject2/js/state-data1.js
@@ -0,0 +1,51 @@
+// object with NYC covid-19 data key:value for each borough in NYC 
+var nysData = {
+	nyc: {
+		name: 'New York City',
+		cases: '698,751',
+		deaths: '29,173',
+		siteName: 'Choose a borough',
+		siteLocation: 'Choose a borough',
+		siteHours: 'Choose a borough'
+	},
+	queens: {
+		name: 'Queens',
+		cases: '203,281',
+		deaths: '8,724',
+		siteName: 'Astoria Welfare Society',
+		siteLocation: '29-11 36th Avenue Astoria, NY 11106',
+		siteHours: 'Friday, 8:00 AM - 7:00 PM'
+	},
+	brooklyn: {
+		name: 'Brooklyn',
+		cases: '201,582',
+		deaths: '9,051',
+		siteName: 'The FARM',
+		siteLocation: '1432 Pitkin Avenue Brooklyn, NY 11233',
+		siteHours: 'Tuesday, Thursday, 10:00 AM - 4:00 PM'
+	},
+	bronx: {
+		name: 'The Bronx',
+		cases: '135,625',
+		deaths: '5,835',
+		siteName: 'Bronx NeON',
+		siteLocation: '198 E. 161st Street Bronx, NY 10451',
+		siteHours: 'Monday - Tuesday, 9:30 AM - 4:00 PM'
+	},
+	manhattan: {
+		name: 'Manhattan',
+		cases: '102,666',
+		deaths: '3,875',
+		siteName: 'Convent Avenue Baptist Church',
+		siteLocation: '420 W 145th St New York, NY 10031',
+		siteHours: 'Monday - Thursday, 9:30 AM - 4:00 PM'
+	},
+	staten: {
+		name: 'Staten Island',
+		cases: '55,597',
+		deaths: '1,571',
+		siteName: 'Tompkinsville Park',
+		siteLocation: '45 Victory Blvd Staten Island, NY 10301',
+		siteHours: 'Tuesday, 8:00 AM - 7:00 PM'
+	}
+};
